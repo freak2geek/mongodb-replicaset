1c76661411b977bbcfb62587191a6366ab0144e3
Nacho Codoner <igcogi@gmail.com>
2023-11-20 18:25:27 +0100

    small script to test

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-printenv
+# Check if MongoDB is running on the specified host and port
+mongo --url $MONGO_URL_ONE --eval "print('Connected to MongoDB')"
+
+if [ $? -eq 0 ]; then
+  echo "MongoDB nodes are available."
+else
+  echo "MongoDB nodes are not available."
+fi
